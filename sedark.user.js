24ed2555122b79e3f5c37a71e95fd5cdf1c1cdd1
zacharee <zachary.wander@gmail.com>
2016-09-19 16:18:42 -0400

    Changed host for CSS

--- sedark.user.js
+++ sedark.user.js
@@ -20,17 +20,17 @@ var spl = str.split("/");
 
 if ((spl[2] == "askubuntu.com") || (spl[2] == "meta.askubuntu.com")) {
     (function() {
-        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://preview.c9users.io/zacharee1/sedark/audark.css" class="--apng-checked">');
+        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://github.com/zacharee/se-dark/raw/master/audark.css" class="--apng-checked">');
     
     })();
 } else if ((spl[2] == "stackoverflow.com") || (spl[2] == "meta.stackoverflow.com")) {
     (function() {
-        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://preview.c9users.io/zacharee1/sedark/sodark.css" class="--apng-checked">');
+        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://github.com/zacharee/se-dark/raw/master/sodark.css" class="--apng-checked">');
     
     })();
 } else if (spl[2] == "chat.stackexchange.com") {
     (function() {
-        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://preview.c9users.io/zacharee1/sedark/sechatdark.css" class="--apng-checked">');
+        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://github.com/zacharee/se-dark/raw/master/sechatdark.css" class="--apng-checked">');
     
     })();
 } else {}
