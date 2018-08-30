9b611bdc4a919637e26c23a1ccc3d31cd7a8a61e
zachary <zachary.wander@gmail.com>
2018-08-30 19:29:48 -0400

    Fix labelling, try unified version

--- sedark.user.js
+++ sedark.user.js
@@ -16,21 +16,26 @@
 
 /*global jQuery*/
 
-var str = window.location.href;
+const str = window.location.href;
 
-var spl = str.split("/");
+const spl = str.split("/");
 
-if ((spl[2] == "askubuntu.com") || (spl[2] == "meta.askubuntu.com")) {
+if ((spl[2] === "askubuntu.com")
+    || (spl[2] === "meta.askubuntu.com")
+    || (spl[2] === "stackoverflow.com")
+    || (spl[2] === "meta.stackoverflow.com")) {
     (function() {
         jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/audark.css" class="--apng-checked">');
     
     })();
-} else if ((spl[2] == "stackoverflow.com") || (spl[2] == "meta.stackoverflow.com")) {
-    (function() {
-        jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/sodark.css" class="--apng-checked">');
-    
-    })();
-} else if (spl[2] == "chat.stackexchange.com" || spl[2] == "chat.stackoverflow.com") {
+}
+// else if ((spl[2] === "stackoverflow.com") || (spl[2] === "meta.stackoverflow.com")) {
+//     (function() {
+//         jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/sodark.css" class="--apng-checked">');
+//
+//     })();
+// }
+else if (spl[2] === "chat.stackexchange.com" || spl[2] === "chat.stackoverflow.com") {
     (function() {
         jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/sechatdark.css" class="--apng-checked">');
     
