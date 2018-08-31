cdf5d440de66eb2a23548d4ee026e82588242759
zachary <zachary.wander@gmail.com>
2018-08-31 06:24:02 -0400

    Cleanup

--- sedark.user.js
+++ sedark.user.js
@@ -67,16 +67,9 @@ if (sites.includes(spl[2])) {
         jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/audark.css" class="--apng-checked">');
     
     })();
-}
-// else if ((spl[2] === "stackoverflow.com") || (spl[2] === "meta.stackoverflow.com")) {
-//     (function() {
-//         jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/sodark.css" class="--apng-checked">');
-//
-//     })();
-// }
-else if (spl[2] === "chat.stackexchange.com" || spl[2] === "chat.stackoverflow.com") {
+} else if (spl[2] === "chat.stackexchange.com" || spl[2] === "chat.stackoverflow.com") {
     (function() {
         jQuery('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/zacharee/se-dark/master/sechatdark.css" class="--apng-checked">');
     
     })();
-} else {}
+}
